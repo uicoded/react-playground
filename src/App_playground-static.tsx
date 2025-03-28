b117fc8d471510ac6fabd7cca1bcdaf87f058536
Jan <uicoded@gmail.com>
2025-03-28 16:44:37 -0400

    Extract the static playground into own component

---
+++ src/App_playground-static.tsx
@@ -0,0 +1,24 @@
+// Swap this 👇 for the example app that you're testing
+import App from './App_default.tsx'
+// import App from './examples/forms/App_forms-uncontrolled-inputs-default-value'
+// import App from './examples/App_error-boundary.tsx'
+// import App from './examples/hooks/use-state/App_hooks-use-state.tsx'
+// import App from './examples/hooks/use-state/App_hooks-use-state-initial-value.tsx'
+// import App from './examples/hooks/use-effect/App_hooks-use-effect.tsx'
+// import App from './examples/hooks/use-effect/App_hooks-use-effect-scenarios.tsx'
+// import App from './examples/hooks/use-effect/App_hooks-use-effect-conditional.tsx'
+// import App from './examples/hooks/use-effect/App_hooks-use-effect-set-interval.tsx'
+// import App from './examples/hooks/use-ref/App_hooks-use-ref-nw.tsx'
+// import App from './examples/hooks/use-ref/App_hooks-use-ref.tsx'
+// import App from './examples/hooks/use-id/App_hooks-use-id.tsx'
+// import App from './examples/styles/App_3-ways-adding-style.tsx'
+// import './examples/styles/App_styled-box.css';
+// import App from './examples/styles/App_styled-box.tsx'
+// import App from './examples/App_using-keys.tsx'
+// import App from './examples/local-storage/App_local-storage-dynamic-value.tsx'
+// import App from './examples/local-storage/App_local-storage-hook.tsx'
+// import App from './examples/local-storage/App_local-storage-immediate-value.tsx'
+
+export default function Playground() {
+  return <App />
+}

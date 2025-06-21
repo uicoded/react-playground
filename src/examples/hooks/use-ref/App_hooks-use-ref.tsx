4ecc11006e3732b93aabd52316ed02161681d2c2
Jan <uicoded@gmail.com>
2025-06-20 22:29:37 -0400

    useRef remarks

--- src/examples/hooks/use-ref/App_hooks-use-ref.tsx
+++ src/examples/hooks/use-ref/App_hooks-use-ref.tsx
@@ -7,7 +7,7 @@ export default function App() {
   useEffect(() => {
     const testDiv = testDivRef.current;
     // testDivRef is the div DOM node!
-    console.log('`testDiv` loaded with: ', testDiv);
+    console.log("`testDiv` loaded with: ", testDiv);
   }, []);
 
   let inputElement: HTMLInputElement | null = null;
@@ -15,10 +15,10 @@ export default function App() {
   // Callback ref
   const setInputFn = (element: HTMLInputElement) => {
     inputElement = element;
-    console.log('`setInputFn` called and `inputElement` loaded with:', element);
+    console.log("`setInputFn` called and `inputElement` loaded with:", element);
     return function cleanup() {
-      console.log('the input is getting removed from page');
-    }
+      console.log("the input is getting removed from page");
+    };
   };
 
   useEffect(() => {
@@ -31,18 +31,36 @@ export default function App() {
     <>
       <h1>ref</h1>
       <p>
+        Refs let a component{" "}
+        <a href="https://react.dev/learn/referencing-values-with-refs">
+          {" "}
+          hold some information that isn’t used for rendering
+        </a>
+        , like a DOM node or a timeout ID. Unlike with state, updating a ref does not re-render your component. Refs are
+        an “escape hatch” from the React paradigm. They are useful when you need to work with non-React systems, such as
+        the built-in browser APIs.
+      </p>
+      <p>
         You can use an object created by <code>useRef</code>
       </p>
-      <div ref={testDivRef} className="testBlock">test block</div>
-      <p> Or you can provide a function that React will call with the DOM element
-        or component instance when it is mounted or updated.
+      <div ref={testDivRef} className="testBlock">
+        test block
+      </div>
+      <p>
+        {" "}
+        Or you can provide a function that React will call with the DOM element or component instance when it is mounted
+        or updated.
         {showInput && <input ref={setInputFn} type="text" />}
       </p>
-      <p>On the input <button onClick={()=>setShowInput(false)}>removal</button> you should see the cleanup
-      function executed.</p>
-      <p>If the component using the ref unmounts normally (through React's lifecycle), React would call the callback with null,
-      triggering any cleanup logic. But when manually removing the element from the DOM using remove(), 
-      React's ref system isn't aware of this change.</p>
+      <p>
+        On the input <button onClick={() => setShowInput(false)}>removal</button> you should see the cleanup function
+        executed.
+      </p>
+      <p>
+        If the component using the ref unmounts normally (through React's lifecycle), React would call the callback with
+        null, triggering any cleanup logic. But when manually removing the element from the DOM using remove(), React's
+        ref system isn't aware of this change.
+      </p>
     </>
   );
 }
